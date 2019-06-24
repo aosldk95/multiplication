bfee3fc2ae6c088bc3a28039be69995d8de7b39e
강병안 <aosldk95@naver.com>
2019-06-24 17:40:13 +0900

    example

--- multiplication.py
+++ multiplication.py
@@ -1,2 +1,20 @@
 # 2520은 1부터 10까지의 숫자로 모두 나눠지는 최소의 수입니다.
 # 1부터 20까지의 숫자로 모두 나눠지는 최소의 수를 구하세요.
+number = 20
+numberlist = []
+j = 2
+for i in range (2, number +1) :
+    line = []
+    while j < i + 1 :
+        if (i % j) == 0 :
+            line.append(j)
+            i /= j
+            j = 2
+        else :
+            j += 1
+    numberlist.append(line)
+print(numberlist)
+
+            
+            
+    
